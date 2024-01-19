2955babc8aa91e46ef7bbeeb26b4ef88179ebd7e
Admin <paresh.makwana.18e@gmail.com>
2024-01-19 15:47:38 +0530

    Traffic + Protocol Analysis added

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import pandas as pd
 from scapy.utils import corrupt_bytes
 # from scapy.layers.inet import IP,TCP,UDP,
 from utils.pcap_decode import PcapDecode
+import time
+
 
 PD = PcapDecode()  # Parser
 PCAPS = None  # Packets
@@ -190,6 +192,163 @@ def dns_stats_main(PCAPS):
     return dns_key_list, dns_value_list
 
 
+
+
+
+def time_flow(PCAPS):
+    time_flow_dict = collections.OrderedDict()
+    start = PCAPS[0].time
+    time_flow_dict[time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(PCAPS[0].time)))] = len(corrupt_bytes(PCAPS[0]))
+    for pcap in PCAPS:
+        timediff = pcap.time - start
+        time_flow_dict[float('%.3f'%timediff)] = len(corrupt_bytes(pcap))
+    return time_flow_dict
+
+
+def get_host_ip(PCAPS):
+    ip_list = list()
+    for pcap in PCAPS:
+        if pcap.haslayer("IP"):
+            ip_list.append(pcap.getlayer("IP").src)
+            ip_list.append(pcap.getlayer("IP").dst)
+    host_ip = collections.Counter(ip_list).most_common(1)[0][0]
+    return host_ip
+
+
+
+def data_flow(PCAPS, host_ip):
+    data_flow_dict = {'IN': 0, 'OUT': 0}
+    for pcap in PCAPS:
+        if pcap.haslayer("IP"):
+            if pcap.getlayer("IP").src == host_ip:
+                data_flow_dict['OUT'] += 1
+            elif pcap.getlayer("IP").dst == host_ip:
+                data_flow_dict['IN'] += 1
+            else:
+                pass
+    return data_flow_dict
+
+
+def data_in_out_ip(PCAPS, host_ip):
+    in_ip_packet_dict = dict()
+    in_ip_len_dict = dict()
+    out_ip_packet_dict = dict()
+    out_ip_len_dict = dict()
+    for pcap in PCAPS:
+        if pcap.haslayer("IP"):
+            dst = pcap.getlayer("IP").dst
+            src = pcap.getlayer("IP").src
+            pcap_len = len(corrupt_bytes(pcap))
+            if dst == host_ip:
+                if src in in_ip_packet_dict:
+                    in_ip_packet_dict[src] += 1
+                    in_ip_len_dict[src] += pcap_len
+                else:
+                    in_ip_packet_dict[src] = 1
+                    in_ip_len_dict[src] = pcap_len
+            elif src == host_ip:
+                if dst in out_ip_packet_dict:
+                    out_ip_packet_dict[dst] += 1
+                    out_ip_len_dict[dst] += pcap_len
+                else:
+                    out_ip_packet_dict[dst] = 1
+                    out_ip_len_dict[dst] = pcap_len
+            else:
+                pass
+
+    in_packet_dict = in_ip_packet_dict
+    in_len_dict = in_ip_len_dict
+    out_packet_dict = out_ip_packet_dict
+    out_len_dict = out_ip_len_dict
+    in_packet_dict = sorted(in_packet_dict.items(), key=lambda d:d[1], reverse=False)
+    in_len_dict = sorted(in_len_dict.items(), key=lambda d:d[1], reverse=False)
+    out_packet_dict = sorted(out_packet_dict.items(), key=lambda d:d[1], reverse=False)
+    out_len_dict = sorted(out_len_dict.items(), key=lambda d:d[1], reverse=False)
+    in_keyp_list = list()
+    in_packet_list = list()
+    for key, value in in_packet_dict:
+        in_keyp_list.append(key)
+        in_packet_list.append(value)
+    in_keyl_list = list()
+    in_len_list = list()
+    for key, value in in_len_dict:
+        in_keyl_list.append(key)
+        in_len_list.append(value)
+    out_keyp_list = list()
+    out_packet_list = list()
+    for key, value in out_packet_dict:
+        out_keyp_list.append(key)
+        out_packet_list.append(value)
+    out_keyl_list = list()
+    out_len_list = list()
+    for key, value in out_len_dict:
+        out_keyl_list.append(key)
+        out_len_list.append(value)
+    in_ip_dict = {'in_keyp': in_keyp_list, 'in_packet': in_packet_list, 'in_keyl': in_keyl_list, 'in_len': in_len_list, 'out_keyp': out_keyp_list, 'out_packet': out_packet_list, 'out_keyl': out_keyl_list, 'out_len': out_len_list}
+    return in_ip_dict
+
+
+def proto_flow(PCAPS):
+    proto_flow_dict = collections.OrderedDict()
+    proto_flow_dict['IP'] = 0
+    proto_flow_dict['IPv6'] = 0
+    proto_flow_dict['TCP'] = 0
+    proto_flow_dict['UDP'] = 0
+    proto_flow_dict['ARP'] = 0
+    proto_flow_dict['ICMP'] = 0
+    proto_flow_dict['DNS'] = 0
+    proto_flow_dict['HTTP'] = 0
+    proto_flow_dict['HTTPS'] = 0
+    proto_flow_dict['Others'] = 0
+    for pcap in PCAPS:
+        pcap_len = len(corrupt_bytes(pcap))
+        if pcap.haslayer("IP"):
+            proto_flow_dict['IP'] += pcap_len
+        elif pcap.haslayer("IPv6"):
+            proto_flow_dict['IPv6'] += pcap_len
+        if pcap.haslayer("TCP"):
+            proto_flow_dict['TCP'] += pcap_len
+        elif pcap.haslayer("UDP"):
+            proto_flow_dict['UDP'] += pcap_len
+        if pcap.haslayer("ARP"):
+            proto_flow_dict['ARP'] += pcap_len
+        elif pcap.haslayer("ICMP"):
+            proto_flow_dict['ICMP'] += pcap_len
+        elif pcap.haslayer("DNS"):
+            proto_flow_dict['DNS'] += pcap_len
+        elif pcap.haslayer("TCP"):
+            tcp = pcap.getlayer("TCP")
+            dport = tcp.dport
+            sport = tcp.sport
+            if dport == 80 or sport == 80:
+                proto_flow_dict['HTTP'] += pcap_len
+            elif dport == 443 or sport == 443:
+                proto_flow_dict['HTTPS'] += pcap_len
+            else:
+                proto_flow_dict['Others'] += pcap_len
+        elif pcap.haslayer("UDP"):
+            udp = pcap.getlayer("UDP")
+            dport = udp.dport
+            sport = udp.sport
+            if dport == 5353 or sport == 5353:
+                proto_flow_dict['DNS'] += pcap_len
+            else:
+                proto_flow_dict['Others'] += pcap_len
+        elif pcap.haslayer("ICMPv6ND_NS"):
+            proto_flow_dict['ICMP'] += pcap_len
+        else:
+            proto_flow_dict['Others'] += pcap_len
+    return proto_flow_dict
+
+def most_flow_statistic(PCAPS, PD):
+    most_flow_dict = collections.defaultdict(int)
+    for pcap in PCAPS:
+        data = PD.ether_decode(pcap)
+        most_flow_dict[data['Procotol']] += len(corrupt_bytes(pcap))
+    return most_flow_dict
+
+
+
 def main():
     st.title("File Upload App")
 
@@ -214,22 +373,53 @@ def main():
             data_protocol_stats=common_proto_statistic(pcap_data )# count the occurrences of common network protocols
             data_count_dict = most_proto_statistic(pcap_data, PD) # counts the occurrences of each protocol and returns most common 10 protocols.
             http_key,http_value = https_stats_main(pcap_data)   #https Protocol Statistics
-            dns_kry,dns_value = dns_stats_main(pcap_data)   #DNS Protocol Statistics
-
+            dns_key,dns_value = dns_stats_main(pcap_data)   #DNS Protocol Statistics
             # Data Protocol analysis end
 
 
 
 
             # Traffic analysis start
-
-
+            time_flow_dict = time_flow(pcap_data)
+            host_ip = get_host_ip(pcap_data)
+            data_flow_dict = data_flow(pcap_data, host_ip)
+            data_ip_dict = data_in_out_ip(pcap_data, host_ip)
+            proto_flow_dict = proto_flow(pcap_data)
+            most_flow_dict = most_flow_statistic(pcap_data, PD)
+            most_flow_dict = sorted(most_flow_dict.items(),key=lambda d: d[1], reverse=True)
+            if len(most_flow_dict) > 10:
+                most_flow_dict = most_flow_dict[0:10]
+            most_flow_key = list()
+            for key, value in most_flow_dict:
+                most_flow_key.append(key)
             # Traffic analysis end
 
 
 
+            # Test area *************************************
+            # Data Protocol analysis
+            print('data_len_stats', data_len_stats)
+            print('data_protocol_stats', data_protocol_stats)
+            print('data_count_dict', data_count_dict)
+            print('http_key', http_key)
+            print('http_value', http_value)
+            print('dns_key', dns_key)
+            print('dns_value', dns_value)
 
-            print(http_key)
+
+
+            # TRaffic analysis
+            print('time_flow_dict--->',time_flow_dict)
+            print('host_ip--->', host_ip)
+            print('data_flow_dict--->', data_flow_dict)
+            print('data_ip_dict--->', data_ip_dict)
+            print('proto_flow_dict--->', proto_flow_dict)
+            print('most_flow_dict--->', most_flow_dict)
+            # print('most_flow_key', most_flow_key)
+
+
+
+            # ***********************************************
 
             # convert data to df
             dataframe_data = process_json_data(all_data)
